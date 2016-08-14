209f572098d171648d229ebda3b5b6bcfabc92b5
Tchowa Franck <francktchowa@gmail.com>
2016-08-15 00:57:53 +0100

    nouvelle interface

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,15 +1,23 @@
 $(document).ready(function(){
-	$('.carousel').carousel({
-		duration: 5000
+	$('ul.nav li').on('click', function(evt){
+		$('ul.nav li.active').removeClass('active');
+		$(this).addClass('active');
 	});
+	$('.carousel').carousel({
+		duration: 3000
+	});
+	//wow js
+	(() => {
+		new WOW().init();
+	})();
 	//smooth scroll of the site
 	$('.carousel-caption a').on('click', function(evt){
 		if(this.hash !== ""){
 			evt.preventDefault();
 			var hash = this.hash;
 			$('html, body').animate({
-				scrollTop: $(hash).offset.top
-			}, 800, function(){
+				scrollTop: $(hash).offset().top
+			}, 1000, function(){
 				window.location.hash = hash;
 			});
 		}
--- routes/contacts.js
+++ routes/contacts.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('contact', { title: 'contact' });
+  res.render('contact', { title: 'Contacts' });
 });
 
 module.exports = router;
